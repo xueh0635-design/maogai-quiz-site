a3ca1f19d7443d88c0c3eb5986f6ea1b08a1418c
xueh0635-design <xueh0635@gmail.com>
2026-05-22 17:40:27 +0800

    Create sync-config.js

---
+++ sync-config.js
@@ -0,0 +1,6 @@
+window.MAOGAI_SYNC_CONFIG = {
+  enabled: true,
+  databaseUrl: "postgresql://neondb_owner:npg_p5ItcMsN4ldf@ep-purple-wind-ajtmqxzc-pooler.c-3.us-east-2.aws.neon.tech/neondb?channel_binding=require&sslmode=require",
+  tableName: "sync_groups",
+  schemaVersion: 1,
+};
